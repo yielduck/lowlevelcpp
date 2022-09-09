a484f66c59f8ac9ed08049826ce0e71d4f96d119
YielDuck <yielduck@gmail.com>
2022-09-09 12:05:07 +0300

    const const const

--- snippets/05_09_22/sqrt.cpp
+++ snippets/05_09_22/sqrt.cpp
@@ -1,12 +1,12 @@
 namespace my
 {
 
-double abs(double x)
+double abs(double const x)
 {
     return x >= 0. ? x : -x;
 }
 
-double sqrt(double a)
+double sqrt(double const a)
 {
     double x0 = 1.;
     while(true)
@@ -17,7 +17,7 @@ double sqrt(double a)
         if(b == 0 || absdiff / b < 1e-10)
             break;
 
-        double x1 = 0.5 * (x0 + a / x0);
+        double const x1 = 0.5 * (x0 + a / x0);
         x0 = x1;
     }
     return x0;
@@ -30,7 +30,7 @@ double sqrt(double a)
 #include <cmath>
 int main()
 {
-    double x = 2.;
+    double const x = 2.;
     std::cout << std::setprecision(16);
     std::cout <<  my::sqrt(x) << std::endl;
     std::cout << std::sqrt(x) << std::endl;
