1f475f582dc28b46a58c2a89a8addd7dbffc40d2
YielDuck <yielduck@gmail.com>
2022-10-10 23:41:24 +0300

    complete array

--- snippets/10_10_22/array.cpp
+++ snippets/10_10_22/array.cpp
@@ -10,20 +10,35 @@ struct array
 {
     T array[N];
 
-    using value_type = T;
-    using size_type = std::size_t;
+    using      value_type = T;
+    using       size_type = std::size_t;
 
-    using iterator = T *;
-    using const_iterator = T const *;
+    using        iterator = T       *;
+    using  const_iterator = T const *;
 
-    iterator begin() {return array;}
-    iterator end() {return array + N;}
+    using       reference = T       &;
+    using const_reference = T const &;
 
-    size_type size() const {return N;}
-    iterator data() {return array;}
+    T       *       data        ()                  {return array;}
+    T const *       data        ()            const {return array;}
 
-    T       &operator[](size_type i)       {return array[i];}
-    T const &operator[](size_type i) const {return array[i];}
+    size_type       size        ()            const {return N;}
+    bool            empty       ()            const {return N == 0u;}
+
+    iterator        begin       ()                  {return array;}
+    iterator        end         ()                  {return array + N;}
+
+    const_iterator  begin       ()            const {return array;}
+    const_iterator  end         ()            const {return array + N;}
+
+    reference       operator[]  (size_type i)       {return array[i];}
+    const_reference operator[]  (size_type i) const {return array[i];}
+
+    reference       front       ()                  {return array[0];}
+    reference       back        ()                  {return array[N - 1u];}
+
+    const_reference front       ()            const {return array[0];}
+    const_reference back        ()            const {return array[N - 1u];}
 };
 
 }
@@ -35,6 +50,7 @@ int main()
     my::array<Jopa, 6> a = {5, 4, 3, 2, 1, 0};
 
     int sum = std::accumulate(a.begin(), a.end(), 0);
+    std::cout << sum << std::endl;
     std::sort(a.begin(), a.end());
 
     for(int i : a)
