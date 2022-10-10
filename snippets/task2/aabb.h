8081c779677895d81a801c4320ed2f791eccb4e9
YielDuck <yielduck@gmail.com>
2022-10-10 23:01:44 +0300

    use min/max in aabb

--- snippets/task2/aabb.h
+++ snippets/task2/aabb.h
@@ -10,20 +10,20 @@ inline MaybeTwoIntersections rayAABBIntersection(Ray const &ray, AABB const &aab
 {
     vec3 const a = (aabb.min - ray.origin) / ray.direction;
     vec3 const b = (aabb.max - ray.origin) / ray.direction;
+    auto const min = [](float const x, float const y) {return x < y ? x : y;};
+    auto const max = [](float const x, float const y) {return x < y ? y : x;};
     vec3 const tMin =
     {
-        a.x < b.x ? a.x : b.x,
-        a.y < b.y ? a.y : b.y,
-        a.z < b.z ? a.z : b.z,
+        min(a.x, b.x),
+        min(a.y, b.y),
+        min(a.z, b.z),
     };
     vec3 const tMax =
     {
-        a.x < b.x ? b.x : a.x,
-        a.y < b.y ? b.y : a.y,
-        a.z < b.z ? b.z : a.z,
+        max(a.x, b.x),
+        max(a.y, b.y),
+        max(a.z, b.z),
     };
-    auto const min = [](float const x, float const y) {return x < y ? x : y;};
-    auto const max = [](float const x, float const y) {return x < y ? y : x;};
     return
     {
         max(max(tMin.x, tMin.y), tMin.z),
