bc1efe0d0736a1fd0bfc8ced896c37b314a9c2fc
YielDuck <yielduck@gmail.com>
2022-10-24 23:13:22 +0300

    prettify task6

--- snippets/24_10_22/task6.cpp
+++ snippets/24_10_22/task6.cpp
@@ -4,9 +4,8 @@ int main()
 {
     char field[13] = {};
 
-    for(int i = 0; i < 3; ++i)
-        for(int j = 0; j < 4; ++j)
-            field[4 * i + j] = std::cin.get();
+    for(int i = 0; i < 12; ++i)
+        field[i] = std::cin.get();
 
     // field: 0 1  2 [ 3]
     //        4 5  6 [ 7]
@@ -26,14 +25,12 @@ int main()
         {2, 5,  8},
     };
     for(int i = 0; i < 8; ++i)
-    {
-        if(field[pattern[i][0]] + field[pattern[i][1]] + field[pattern[i][2]] != 'x' + 'x' + ' ')
-            continue;
-
-        for(int j = 0; j < 3; ++j)
-            field[pattern[i][j]] = 'x';
-        std::cout << field << std::endl;
-        return 0;
-    }
+        if(field[pattern[i][0]] + field[pattern[i][1]] + field[pattern[i][2]] == 'x' + 'x' + ' ')
+        {
+            for(int j = 0; j < 3; ++j)
+                field[pattern[i][j]] = 'x';
+            std::cout << field << std::endl;
+            return 0;
+        }
     std::cout << -1 << std::endl;
 }
